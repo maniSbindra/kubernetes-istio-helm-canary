71ab4d0ad9f42123058a54b4dae283c33da56c5c
manisbindra <maninder.bindra@gmail.com>
2019-03-28 20:23:58 +0530

    adding canary deployment to chart and modifying welcome message

--- productpage/productpage.py
+++ productpage/productpage.py
@@ -188,7 +188,7 @@ def getProducts():
     return [
         {
             'id': 0,
-            'title': 'The Comedy of Errors - Hello Mumtechup',
+            'title': 'The Comedy of Errors - Hello V1.1',
             'descriptionHtml': '<a href="https://en.wikipedia.org/wiki/The_Comedy_of_Errors">Wikipedia Summary</a>: The Comedy of Errors is one of <b>William Shakespeare\'s</b> early plays. It is his shortest and one of his most farcical comedies, with a major part of the humour coming from slapstick and mistaken identity, in addition to puns and word play.'
         }
     ]
